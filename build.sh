dc9997b28fc127113f6dc0172d3fc8bbebf73c62
Robert Xu <me@robxu9.com>
2021-11-13 21:04:47 -0500

    Build different arches separately (whoops)

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # use musl-cross-make and build
-# invoke: ./build.sh [config variant] [previous-toolchain, if any]
+# invoke: ./build.sh [config variant] [target] [previous-toolchain, if any]
 
 # exit on failure
 set -e
 
 # copy the config
 cp -v config.mak.$1 musl-cross-make/config.mak
+sed -i s/__TARGET__/$2/ musl-cross-make/config.mak
 
 # if we have a previous toolchain, set it in the path
-if [ ! -z $2 ]; then
-  export PATH="$(realpath $2):$PATH"
+if [ ! -z $3 ]; then
+  export PATH="$(realpath $3):$PATH"
 fi
 
 # kickoff the build

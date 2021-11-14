79eb2d0b008691b6163da858b9cc9c63e690a747
Robert Xu <me@robxu9.com>
2021-11-13 23:55:33 -0500

    Consolidate configs, NATIVE=1

--- build.sh
+++ build.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 # use musl-cross-make and build
-# invoke: ./build.sh [config variant] [target] [previous-toolchain, if any]
+# invoke: ./build.sh [target] [cross-toolchain, if any]
 
-# exit on failure
-set -e
+# exit on failure, show commands
+set -ex
 
 # copy the config
-cp -v config.mak.$1 musl-cross-make/config.mak
-sed -i s/__TARGET__/$2/g musl-cross-make/config.mak
+cp -v config.mak musl-cross-make/config.mak
+sed -i s/__TARGET__/$1/g musl-cross-make/config.mak
 
-# if we have a previous toolchain, set it in the path
-if [ ! -z $3 ]; then
-  export PATH="$(realpath $3)/bin:$PATH"
+# if we have a cross toolchain, set it in the path
+if [ ! -z $2 ]; then
+  export PATH="$(realpath $2)/bin:$PATH"
 fi
 
 # kickoff the build
 cd musl-cross-make
-make
+if [ ! -z $2 ]; then
+  make NATIVE=1
+else
+  make
+fi
 make install
--- config.mak
+++ config.mak
@@ -5,4 +5,7 @@
 TARGET = __TARGET__
 
 COMMON_CONFIG += CFLAGS="-g0 -Os" CXXFLAGS="-g0 -Os" LDFLAGS="-s"
+
+ifdef $(NATIVE)
 COMMON_CONFIG += CC="__TARGET__-gcc -static --static" CXX="__TARGET__-g++ -static --static"
+endif
